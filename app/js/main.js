7189baf9639d3cd3d0fc6b5d3c40b54b77a22ddc
Vadim Derman <thenoodly@gmail.com>
2016-03-23 21:24:46 -0700

    finished layout

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+(function() {
+    var friendWindow = document.getElementById('allFriends');
+    var friendWindow2 = document.getElementById('addedFriends');
+
+    Ps.initialize(friendWindow);
+    Ps.initialize(friendWindow2);
+}());
